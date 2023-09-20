e4839e9fb1a6ac97be9f41e07eb9290b47deb49a
Marino <marinoj.business@gmail.com>
2023-09-20 15:30:41 +0200

    renamed shaders(changed extensions) and edited cmakelists to account for changes

--- src/Room-Planner/main.cpp
+++ src/Room-Planner/main.cpp
@@ -31,7 +31,7 @@ const unsigned int SCR_WIDTH = 1920;
 const unsigned int SCR_HEIGHT = 1080;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 7.0f, 5.0f), glm::vec3(0.0f,1.0f,0.0f),-90.0f,-45.0f);
 Camera backupCamera;
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
@@ -44,6 +44,7 @@ bool imguiMode = false;
 // timing
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
+
 
 int main()
 {
@@ -60,7 +61,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Room Planner", glfwGetPrimaryMonitor(), NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Room Planner", /*glfwGetPrimaryMonitor()*/NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -103,20 +104,52 @@ int main()
 
     // build and compile shaders
     // -------------------------
-    Shader ourShader("vertex.vs", "fragment.fs");
+    Shader wallShader("wall_vertex.vert", "wall_fragment.frag");
+    Shader modelShader("model_vertex.vert", "model_fragment.frag");
 
-    // load models
-    // -----------
-    Model ourModel(FileSystem::getPath("resources/objects/nanosuit/nanosuit.obj"));
+    float length, width;
+    bool walls_created = false;
+    float* wall_vertices;
+    GLuint VAO_walls, VBO_walls, EBO_walls;
+    glGenVertexArrays(1, &VAO_walls);
+    glGenBuffers(1, &VBO_walls);
+    glGenBuffers(1, &EBO_walls);
 
     
 
-    // draw in wireframe
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    std::vector<GLuint> wall_indices{
+        //face wall
+        2,3,7,
+        2,6,7,
+        //rear wall
+        0,1,5,
+        0,4,5,
+        //left wall
+        0,2,6,
+        0,4,6,
+        //right wall
+        1,3,7,
+        1,5,7,
+        //floor
+        0,1,2,
+        1,2,3
+    };
+
+    
+
+    // load models
+    // -----------
+    Model ourModel(FileSystem::getPath("resources/objects/backpack/backpack.obj"));
+    std::vector<Model *> models;
 
     // render loop
     // -----------
     glm::vec3 translate(glm::vec3(0.0f, 0.0f, 0.0f));
+
+    float minLength = 5.0f; // Minimum length value
+    float minWidth = 5.0f;  // Minimum width value
+    float minHeight = 3.0f; // Minimum height value
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
@@ -130,6 +163,50 @@ int main()
         {
             ImGui::SliderFloat3("Translation", &translate.x, -100.0f, 100.0f);
             ImGui::Text("Application avg %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            // ImGui input fields
+            ImGui::InputFloat("Length", &length, 0.1f, 1.0f, "%.2f", ImGuiInputTextFlags_CharsDecimal);
+            ImGui::InputFloat("Width", &width, 0.1f, 1.0f, "%.2f", ImGuiInputTextFlags_CharsDecimal);
+
+            // Clamp values to minimum values
+            length = (length < minLength) ? minLength : length;
+            width = (width < minWidth) ? minWidth : width;
+            if (!walls_created) {
+                if (ImGui::Button("Create walls")) {
+                    walls_created = true;
+                    wall_vertices = new float[24] {
+                        //floor points
+                        -length / 2,0.0f,width / 2, //top left
+                        length / 2,0.0f,width / 2,  //top right
+                        -length / 2,0.0f,-width / 2, //bottom left
+                        length / 2,0.0f,-width / 2, //bottom right
+
+                        //roof points
+                        -length / 2,3.0f,width / 2, //top left
+                        length / 2,3.0f,width / 2,  //top right
+                        -length / 2,3.0f,-width / 2, //bottom left
+                        length / 2,3.0f,-width / 2, //bottom right
+                    };
+
+                    glBindVertexArray(VAO_walls);
+
+                    glBindBuffer(GL_ARRAY_BUFFER, VBO_walls);
+                    glBufferData(GL_ARRAY_BUFFER, 24 * sizeof(float) , wall_vertices, GL_STATIC_DRAW);
+
+                    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_walls);
+                    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(wall_indices) * sizeof(GLuint), wall_indices.data(), GL_STATIC_DRAW);
+
+                    wallShader.use();
+                    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+                    glEnableVertexAttribArray(0);
+
+                    glBindBuffer(GL_ARRAY_BUFFER, 0);
+                    glBindVertexArray(0);
+                }
+            }
+            if (ImGui::Button("Add test model")) {
+                models.push_back(&ourModel);
+            }
+            ImGui::Text("Currently loaded %d test models", models.size());
         }
         // per-frame time logic
         // --------------------
@@ -147,22 +224,37 @@ int main()
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // don't forget to enable shader before setting uniforms
-        ourShader.use();
-
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
-        ourShader.setMat4("projection", projection);
-        ourShader.setMat4("view", view);
+       
 
+
+        
+        
+        if (walls_created) {
+            wallShader.use();
+            wallShader.setMat4("projection", projection);
+            wallShader.setMat4("view", view);
+            wallShader.setMat4("model", glm::mat4(1.0f));
+            glBindVertexArray(VAO_walls);
+            glDrawElements(GL_TRIANGLES, wall_indices.size(), GL_UNSIGNED_INT, 0);
+            glBindVertexArray(0);
+        }
+
+        modelShader.use();
+        modelShader.setMat4("projection", projection);
+        modelShader.setMat4("view", view);
+        
         // render the loaded model
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, translate); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
-        ourShader.setMat4("model", model);
-        ourModel.Draw(ourShader);
-
+        for (int i = 0; i < models.size(); i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, translate); // translate it down so it's at the center of the scene
+            model = glm::translate(model, glm::vec3(0.0f + static_cast<float>(i)*3.0f, 0.0f, 0.0f));
+            model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+            modelShader.setMat4("model", model);
+            models[i]->Draw(modelShader);
+        }
 
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
